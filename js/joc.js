80aee5bab74f2a356fb242ee0204e61edad165b4
Marc <mtorressa@edu.tecnocampus.cat>
2026-05-20 12:47:01 +0200

    forma totxos

--- js/joc.js
+++ js/joc.js
@@ -9,37 +9,37 @@ class Joc{
         this.amplada = canvas.width;
         this.alcada = canvas.height;
         this.totxoamplada = 22;
-        this.totxoalcada = 10; // MIDES DEL TOTXO EN PÍXELS
+        this.totxoalcada = 10;
         this.totxocolor = 20;
-       
 
-        this.bola = new Bola(new Punt(this.canvas.width/2,this.canvas.height-20),3);
-        this.pala = new Pala(new Punt((this.canvas.width-60)/2,this.canvas.height-15),60,4);
-        this.totxo = new Totxo(new Punt((this.canvas.width-120)/2,(this.canvas.height-20)/3), 120, 20, "#0ad");  // només posem un totxo gegant
+        this.bola = new Bola(new Punt(this.canvas.width/2, this.canvas.height/2), 3);
+        this.pala = new Pala(new Punt((this.canvas.width-60)/2, this.canvas.height-15), 60, 4);
+        this.mur  = new Mur();
+        this.mur.generaMur(0, this.amplada);
 
         this.key = {
-            LEFT:{code:37, pressed:false},
+            LEFT: {code:37, pressed:false},
             RIGHT:{code:39, pressed:false}
-        };      
+        };
     }
 
     draw(){
         this.clearCanvas();
+        this.mur.draw(this.ctx);
         this.pala.draw(this.ctx);
         this.bola.draw(this.ctx);
-        this.totxo.draw(this.ctx);
-
-
     }
+
     clearCanvas(){
-        this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height)
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
     inicialitza(){
+        this.mur.draw(this.ctx);
         this.pala.draw(this.ctx);
         this.bola.draw(this.ctx);
-        this.totxo.draw(this.ctx);
-        $(document).on("keydown",{joc:this}, function(e){
+
+        $(document).on("keydown", {joc:this}, function(e){
             if (e.which === joc.key.LEFT.code)  joc.key.LEFT.pressed  = true;
             if (e.which === joc.key.RIGHT.code) joc.key.RIGHT.pressed = true;
         });
@@ -47,14 +47,11 @@ class Joc{
             if (e.which === joc.key.LEFT.code)  joc.key.LEFT.pressed  = false;
             if (e.which === joc.key.RIGHT.code) joc.key.RIGHT.pressed = false;
         });
-
-        
     }
 
     update(){
         this.bola.update();
         this.pala.update();
-        this.draw();       
-
+        this.draw();
     }
 }
--- js/mur.js
+++ js/mur.js
@@ -4,48 +4,73 @@
 
 class Mur {
     constructor() {
-        
+        this.totxos = [];
     }
 
-    generaMur(){
-       
+    generaMur(nivell, ampladaCanvas){
+        this.totxos = [];
+        this.defineixNivells();
+        const nivel = this.nivells[nivell];
+        const files = nivel.totxos;
+        const nCols = files[0].length;
+        const nFiles = files.length;
+
+        const tW = 28;   // amplada totxo
+        const tH = 8;    // alçada totxo
+        const gap = 3;   // separació entre totxos
+        const margeTop = 20;
+        const totalAmplada = nCols * (tW + gap) - gap;
+        const margeLeft = (ampladaCanvas - totalAmplada) / 2;
+
+        for (let f = 0; f < nFiles; f++) {
+            for (let c = 0; c < nCols; c++) {
+                if (files[f][c] === 'a') {
+                    const x = margeLeft + c * (tW + gap);
+                    const y = margeTop + f * (tH + gap);
+                    let t = new Totxo(new Punt(x, y), tW, tH);
+                    t.color = nivel.color;
+                    t.punts = 10;
+                    this.totxos.push(t);
+                }
+            }
+        }
     }
+
     draw(ctx){
-       
+        for (let t of this.totxos) {
+            t.draw(ctx);
+        }
     }
-     
+
     defineixNivells(){
         this.nivells=[
             {
-                color: "#4CF", // blue cel
+                color: "#4CF",
                 totxos:[
-                    "aaaaaaaaaaaa",
-                    "aaaaaaaaaaaa",
-                    "aaaaaaaaaaaa",
-                    "aaaaaaaaaaaa",
+                    "aaaaaa",
+                    "aaaaaa",
+                    "aaaaaa",
+                    "aaaaaa",
                 ]
             },
             {
-                color: "#8D1", // verd
+                color: "#8D1",
                 totxos:[
-                    "aaaaaaaaaaaa",
-                    "     aa     ",
-                    "   aaaaaa   ",
-                    "   aaaaaa   ",
-                    "     aa     ",
+                    "aaaaaa",
+                    " aaaa ",
+                    " aaaa ",
+                    "aaaaaa",
                 ]
             },
             {
-                color: "#D30", // vermell
+                color: "#D30",
                 totxos:[
-                    "aaaaaaaaaaaa",
-                    "a          a",
-                    " a        a ",
-                    "aa        aa",
-                    "  aaaaaaaa  ",
+                    "aaaaaa",
+                    "a    a",
+                    "a    a",
+                    "aaaaaa",
                 ]
             }
         ];
     }
-
-};
+}
