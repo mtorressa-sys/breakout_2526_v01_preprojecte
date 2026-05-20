8bcf695dbdd2122f602639f27b6be4d78d614a70
Neus <neusgisbert20@gmail.com>
2026-05-20 13:52:51 +0200

    velocitat pala

--- js/pala.js
+++ js/pala.js
@@ -8,7 +8,7 @@ class Pala {
         this.alcada = alcada;
         this.posicio = puntPosicio;
         this.vy = 2;     
-        this.vx = 8;                                                     // velocitat = 10 píxels per fotograma
+        this.vx = 10;                                                     // velocitat = 10 píxels per fotograma
         this.color = "#D30"; 
     }
 
